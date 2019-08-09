67ad3239b8eeed39cd77f19ce22b97723cfa4d70
stefan.berger <stefan.berger@check24.de>
2019-08-09 11:23:36 +0200

    save pictures as png

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ myGene = trainGenerator(2,'data/train','image','label',data_gen_args,save_to_dir
 def train():
     model = unetNorm()
     model_checkpoint = ModelCheckpoint('unetNorm.hdf5', monitor='loss',verbose=1, save_best_only=True)
-    model.fit_generator(myGene,steps_per_epoch=50,epochs=5000,callbacks=[model_checkpoint])
+    model.fit_generator(myGene,steps_per_epoch=50,epochs=1000,callbacks=[model_checkpoint])
 
 # test model
 def test():
     testGene = testGenerator("data/test")
     model = unetNorm()
-    model.load_weights("unetNorm_0.027_loss.hdf5")
+    model.load_weights("unetNorm.hdf5")
     results = model.predict_generator(testGene,20,verbose=1)
     saveResult("data/result",results)
 
 if __name__ == "__main__":
+    train()
     test()
